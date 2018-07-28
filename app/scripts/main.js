34bbc3353ee954011a098f5cce410c9b461a13d7
SukachevAlex <sukachevalexandr@gmail.com>
2018-07-28 18:21:28 +0300

    make section gallery

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
+const galleryImage = document.querySelector('.gallery-item__image');
+const galleryTitle = document.querySelector('.gallery-item__title');
+const gallerySubTitle = document.querySelector('.gallery-item__sub-title');
+
 document.querySelector('.header-page').style.height = `${window.innerHeight}px`;
+
+const handleClick = (e) => {
+    let element = e.currentTarget.querySelector('.thumbnail__image');
+    galleryImage.style.backgroundImage = `url('${element.src.replace('_thumb', '')}')`;
+    galleryTitle.innerHTML = element.alt;
+    gallerySubTitle.innerHTML = element.dataset.sub;
+}
+
+window.onload = function() {
+    let thumbs = document.querySelectorAll('.thumbnail__item');
+    thumbs.forEach(el => el.addEventListener('click', handleClick));
+
+};
